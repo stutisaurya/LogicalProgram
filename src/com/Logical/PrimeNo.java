51e8b2c1af7e62a977abad0cde0d5e9ccb44f046
stutisaurya <stuti.saurya@gmail.com>
2021-06-20 20:10:34 +0530

    Added:Prime Number

---
+++ src/com/Logical/PrimeNo.java
@@ -0,0 +1,25 @@
+package com.Logical;
+
+import java.util.Scanner;
+
+public class PrimeNo {
+
+	public static void main(String[] args) {
+		int n, count = 0;
+		System.out.println("Enter any Number");
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		for (int i = 1; i <= n; i++) {
+			if (n % i == 0) {
+				count++;
+			}
+
+		}
+		if (count == 2)
+			System.out.println("Prime Number");
+		else
+			System.out.println("Not Prime Number");
+
+	}
+
+}
